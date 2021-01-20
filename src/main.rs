3291c8e89e4c21abdc096d9b5d0ee1af0ce737d1
Juan Tavera <jtavera235@gmail.com>
2021-01-19 23:56:04 -0500

    Added heroku env ports

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::env;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -12,11 +13,17 @@ async fn home() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("New Server is starting up.");
+
+    let port = env::var("PORT")
+        .unwrap_or_else(|_| "3000".to_string())
+        .parse()
+        .expect("PORT must be a number");
+
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .route("/api/v0", web::get().to(home))
-    }).bind(("0.0.0.0", 3000))?
+    }).bind(("0.0.0.0", port))?
         .run()
         .await
 }
