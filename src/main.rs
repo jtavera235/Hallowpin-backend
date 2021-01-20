58ed2a2421c584c6593d2b8308da0f49f38fd9e1
Juan Tavera <jtavera235@gmail.com>
2021-01-19 23:04:00 -0500

    Init actix-web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("hello World")
+}
+
+async fn home() -> impl Responder {
+    HttpResponse::Ok().body("")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .route("/api/v0", web::get().to(home))
+    }).bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
